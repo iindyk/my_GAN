generator's gradient corrected

tf_version_mnist/training.py
@@ -88,12 +88,20 @@ for i in range(n_d_vars):
     new_d_accumulation.append(d_accumulation[i].assign(momentum * d_accumulation[i] + (1.-momentum)*d_grad[i]))
     new_d_vars.append(d_vars[i].assign(d_vars[i] - learning_rate * d_accumulation[i]))
 
+g_jacob_p2 = []
+for j in range(batch_size):
+    g_jacob_p2.append([])
+    for k in range(generator.output_size):
+        g_jacob_p2[j].append([])
+        for l in range(generator.output_size):
+            g_jacob_p2[j][k].append(tf.gradients(xs=g_vars, ys=g_z[j, k, l]))
 g_grad_p2 = []
 for i in range(n_g_vars):
-    g_grad_p2.append(tf.reduce_sum([g_grad_p2_1[j%28, j//28, 0]*tf.gradients(xs=g_vars[i], ys=g_z[j%28, j//28, 0])for j in range(784)], axis=0))
+    print(i)
+    g_grad_p2.append(tf.matmul(tf.reshape(g_grad_p2_1, [-1]), tf.reshape(g_jacob_p2[:][:][:][i])))
     g_accumulation.append(tf.get_variable('accum_g' + str(i), shape=g_grad_p1[i].get_shape(), trainable=False))
     new_g_accumulation.append(g_accumulation[i].assign(momentum * g_accumulation[i] +
-                              (1.-momentum) * (g_grad_p1[i]+generator.alpha*g_grad_p2[i])))    # todo: check shapes
+                              (1.-momentum) * (g_grad_p1[i]+generator.alpha*g_grad_p2[i])))
     new_g_vars.append(g_vars[i].assign(g_vars[i] - learning_rate * g_accumulation[i]))
 
 # Initialize the variables (i.e. assign their default value)