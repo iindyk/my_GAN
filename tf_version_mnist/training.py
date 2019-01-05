generator's gradient corrected

tf_version_mnist/training.py
@@ -90,15 +90,17 @@ for i in range(n_d_vars):
 
 g_jacob_p2 = []
 for j in range(batch_size):
+    print(j)
     g_jacob_p2.append([])
     for k in range(generator.output_size):
         g_jacob_p2[j].append([])
         for l in range(generator.output_size):
             g_jacob_p2[j][k].append(tf.gradients(xs=g_vars, ys=g_z[j, k, l]))
+g_jacob_p2 = tf.convert_to_tensor(g_jacob_p2, dtype=tf.float32)
 g_grad_p2 = []
 for i in range(n_g_vars):
     print(i)
-    g_grad_p2.append(tf.matmul(tf.reshape(g_grad_p2_1, [-1]), tf.reshape(g_jacob_p2[:][:][:][i])))
+    g_grad_p2.append(tf.matmul(tf.reshape(g_grad_p2_1, [-1]), tf.reshape(g_jacob_p2[:, :, :, i]), transpose_a=True))
     g_accumulation.append(tf.get_variable('accum_g' + str(i), shape=g_grad_p1[i].get_shape(), trainable=False))
     new_g_accumulation.append(g_accumulation[i].assign(momentum * g_accumulation[i] +
                               (1.-momentum) * (g_grad_p1[i]+generator.alpha*g_grad_p2[i])))